6dbc16281f964beb80224994cf1f33ed26fd904e
한민정 <a@a.com>
2023-05-22 16:12:44 +0900

    5월 22일 리액트 디데이 계산기 마무리

--- calc/src/components/compDDay/CompList.jsx
+++ calc/src/components/compDDay/CompList.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
 import { DDayContext } from './CompDDay';
 import CompListItem from './CompListItem';
+import CompFilterForm from './CompFilterForm';
 
 const CompList = () => {
+  const [_serchText, _setSearchText] = useState('')
+  const [_filterActive, _setFilterActive] = useState('')
   const {_showComp, _setShowComp, _ddayArr, _setDdayArr,_ddayOutputArr, _setDdayOutputArr} = useContext(DDayContext)
+
+  const fnFilterHandler = function(e){
+    e.currentTarget.classList.toggle('active')
+    _setFilterActive(c => (c !== 'active')? 'active':'')
+    _setSearchText('')
+  }
+
   return (
     <>
       <article className='dday-list'>
+        <button onClick={fnFilterHandler} className='filter-btn'>
+          <i className="fa-solid fa-magnifying-glass-plus"></i>
+          <i className="fa-solid fa-magnifying-glass-minus"></i>
+        </button>
+
         { /* 배열에 객체가 있을 경우 */
           (_ddayOutputArr.length)? 
           <ul>
@@ -18,6 +33,8 @@ const CompList = () => {
             "DDAY일정이 존재하지 않습니다"
           </p>
         }
+
+        <CompFilterForm _filterActive={_filterActive} _setFilterActive={_setFilterActive} _serchText={_serchText} _setSearchText={_setSearchText}/>
       </article>
       <button onClick={()=>{_setShowComp('add')}} className='btn-add'> 
         <img src={require('../../assets/img/timer-icon-bg.gif')} alt="" /> D-Day 추가하기
