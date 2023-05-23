7b19e2c81315712681148614de9201f8e2e3c824
한민정 <a@a.com>
2023-05-23 17:45:37 +0900

    5월 23일 api를 이용한 날씨 리액트 만들기

---
+++ weather-api/src/components/compDaily/CompDaily.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CompDaily = () => {
+  return (
+    <section>
+      <div className='section-inner'>
+        daily
+        
+      </div>
+    </section>
+  );
+};
+
+export default CompDaily;
---
+++ weather-api/src/components/compHourly/CompHourly.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CompHourly = () => {
+  return (
+    <section>
+      <div className='section-inner'>
+        hourly
+        
+      </div>
+    </section>
+  );
+};
+
+export default CompHourly;
